a8ba1c9043e4b76b4031cbe8f59a94213bfab6be
Austin Wagner <austin.e.wagner@gmail.com>
2018-03-12 21:54:40 -0700

    make it better

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -10,7 +10,7 @@ const About = () => (
                     <div className="col-lg-8 mx-auto">
                         <h1 className="brand-heading">About Me</h1>
                         <p>
-                            I am a naturally curious, problem solver who developed valuable skills from experience living and working in China. Examples include strong communication skills, an ability to work in fast paced and chaotic environment, and an appreciation for and ability to work with and in diverse teams.
+                            I am a naturally curious problem solver who developed valuable skills from experience living and working in China. Examples include strong communication skills, an ability to work in fast paced and chaotic environment, and an appreciation for and ability to work with and in diverse teams.
                         </p>
                         <p>
                             My interest in coding began with using statistical software for my graduate school thesis and has continued to grow. Coding is fun and meditative, because it offers an outlet for my obsession with with organization, detail, and numbers.
